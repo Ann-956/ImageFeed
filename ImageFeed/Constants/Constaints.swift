f4b38f68785cb48bbff2beef45037092210d4e3e
Ann@ <ann-brown956@yandex.ru>
2024-06-03 13:28:36 +0700

    изменения из главной ветки

---
+++ ImageFeed/Constants/Constaints.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+enum Constaints {
+    static let accessKey = "-tvtXvmuyASAAQvCPP5QZDJ1ykxY78VEedJghkFJDEE"
+    static let secretKey = "5ZuggQSr5oyCYU-D6V8xrNWY9cZ3_CrgUpkqRoTHHN4"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    static let acccessScope = "public+read_user+write_likes"
+    static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
+}
